f88f76a5bbb4f9d0a3f3f05688c3fc2409ad3e2d
Lopezsa3 <69913666+Lopezsa3@users.noreply.github.com>
2020-08-19 11:50:21 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***********
+*HelloWorld.Java
+***********
+*Steven Lopez
+*Nineteenth of August
+*Programming
+***********/
+public class Hello {
+	public static void main(String[]args){
+		System.out.println ("Steven");
+		System.out.println("Java HelloWorld");
+		System.out.println("A brief intro to Java");
+		System.out.println("8/19/2020");
+		System.out.println("Introduction to programming 004");
+		System.out.println();
+		}
+	}
+		
